4ae6aafc02ed048f9d690433ff07dd6b72169498
Christopher Schuster <schustrchr@gmail.com>
2022-08-13 09:42:49 +0200

    Version increment

--- ctoffer-commons-installer/src/main/java/de/ctoffer/commons/installer/RpiServiceInstaller.java
+++ ctoffer-commons-installer/src/main/java/de/ctoffer/commons/installer/RpiServiceInstaller.java
@@ -1,8 +1,10 @@
 package de.ctoffer.commons.installer;
 
+import de.ctoffer.commons.io.StdIo;
+
 public class RpiServiceInstaller {
     public static void main(String[] args) {
-        System.out.println("RpiInstaller running ...");
+        StdIo.print(System.getProperty("artifactId"), System.getProperty("version"));
 
         // Get pom.xml and MavenProjectStructure object
         // Use final var reader = new MavenXpp3Reader(); to read POM to retrieve artifact id
